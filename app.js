22bd97ebd9418a5cfece6ff2702cc9e82170f1b9
Demo-tong <18829292377@163.com>
2016-07-29 14:59:11 +0800

    shell

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 'use strict';
 
 const repl = require('repl');
+const bTop = require('./transformer/barcodeToPostcode.js');
+const pTob = require('./transformer/postcodeToBarcode.js');
 
 function switchRouter(context, done) {
   let router = actions.find(item => item.name === currentAction);
   let result = router.doAction(context.cmd);
   let newRouter = actions.find(item => item.name === result);
   currentAction = newRouter.name;
+  console.log(newRouter.help);
   done(null);
 }
 
@@ -21,13 +24,19 @@ var replServer = repl.start({prompt: "> ", eval: handleCmd});
 
 const actions = [{
   name: 'init',
-  help: "初始化界面:1-number,2-letter",
-  doAction: function(cmd) {
-    switch(cmd) {
+  help: "\t\t--------------------------\n" +
+  "\t\t||\t   首页\n" +
+  "\t\t|| 1---postcode to barcode\n" +
+  "\t\t|| 2---barcode to postcode\n" +
+  "\t\t|| q---exit\n" +
+  "\t\t--------------------------\n" +
+  "Please input your number:",
+  doAction: function (cmd) {
+    switch (cmd) {
       case '1':
-        return 'number';
+        return 'postcodeToBarcode';
       case '2':
-        return 'letter';
+        return 'barcodeToPostcode';
       case 'q':
         replServer.close();
         process.exit(0);
@@ -38,28 +47,74 @@ const actions = [{
     }
   }
 }, {
-  name: 'letter',
-  help: 'letter的状态',
-  doAction: function(cmd) {
-    switch(cmd) {
-      case 'a':
-        return 'number'
+  name: 'postcodeToBarcode',
+  help: '\t\t-------------------------\n' +
+  '\t\t||\t邮编转条码首页\n' +
+  '\t\t|| 1---input the barcode\n' +
+  '\t\t|| 2---back to previous\n' +
+  '\t\t|| q---exit\n' +
+  '\t\t-------------------------\n' +
+  'Please input your number:',
+  doAction: function (cmd) {
+    switch (cmd) {
+      case '1':
+        return 'input postcode';
+      case '2':
+        return 'init';
+      case 'q':
+        replServer.close();
+        process.exit(0);
+        return;
       default:
         console.log("无效的输入");
-        return 'init'
-    } 
+        return 'postcodeToBarcode';
+    }
   }
 }, {
-  name: 'number',
-  help: "number的状态",
-  doAction: function(cmd) {
-    switch(cmd) {
+  name: 'input postcode',
+  help: "\n\n\t\t\t邮编转条码\nPlease input your postcode(q to exit):",
+  doAction: function (cmd) {
+    if (cmd === 'q') {
+      return 'postcodeToBarcode';
+    }
+    console.log("The result is:");
+    console.log(pTob.transferToBarcode(cmd.trim()));
+    return 'input postcode';
+  }
+}, {
+  name: 'barcodeToPostcode',
+  help: '\t\t-------------------------\n' +
+  '\t\t||\t条码转邮编首页\n' +
+  '\t\t|| 1---input the barcode\n' +
+  '\t\t|| 2---back to previous\n' +
+  '\t\t|| q---exit\n' +
+  '\t\t-------------------------\n' +
+  'Please input your number:',
+  doAction: function (cmd) {
+    switch (cmd) {
       case '1':
-        return 'letter'
+        return 'input barcode';
+      case '2':
+        return 'init';
+      case 'q':
+        replServer.close();
+        process.exit(0);
+        return;
       default:
         console.log("无效的输入");
-        return 'init'
+        return 'barcodeToPostcode';
     }
+  }
+}, {
+  name: 'input barcode',
+  help: "\n\n\t\t\t条码转邮编\nPlease input your barcode(q to exit):",
+  doAction: function (cmd) {
+    if (cmd === 'q') {
+      return 'barcodeToPostcode';
+    }
+    console.log("The result is:");
+    console.log(bTop.transferToPostCode(cmd.trim()));
+    return 'input barcode';
   }
 }];
 
